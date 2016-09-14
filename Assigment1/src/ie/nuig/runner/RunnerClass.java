fb7d453a3901acf79bd0fde5cc08945f11408dde
VytasHub <vytasvaiciulisgti@gmail.com>
2016-09-14 10:12:07 +0100

    Search Algo Working

--- Assigment1/src/ie/nuig/runner/RunnerClass.java
+++ Assigment1/src/ie/nuig/runner/RunnerClass.java
@@ -1,10 +1,40 @@
 package ie.nuig.runner;
 
-public class RunnerClass {
+import java.util.Arrays;
+import java.util.Random;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import ie.nuig.search.SearchAlgo;
 
+public class RunnerClass 
+{
+
+	public static void main(String[] args) 
+	{
+		Random rand = new Random();
+		
+		  int[] anArray = new 	int[20];
+		  int  n;
+		    	for(int i=0;i<anArray.length;i++)
+		    	{
+		    		n = rand.nextInt(50) + 1;
+		    		anArray[i] = n;
+		    	}
+
+		    	System.out.println(Arrays.toString(anArray));
+		   
+		
+		
+		
+		SearchAlgo s = new SearchAlgo();
+		int[] returnedArray = new 	int[20];
+		returnedArray = s.bucketSort(anArray, 20);
+		
+		System.out.println(Arrays.toString(returnedArray));
+		
 	}
+	
+	
+		  
+		
 
 }
