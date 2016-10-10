0bb4c7f6b7f74def8b11a18acbe7777a5fdbcb0e
VytasHub <vytasvaiciulisgti@gmail.com>
2016-10-10 19:02:40 +0100

    J help got me going
    Julianne
    Julija
    Vaiciulis

--- Assignment_2/src/ie/nuig/stattion/Measurement.java
+++ Assignment_2/src/ie/nuig/stattion/Measurement.java
@@ -11,6 +11,7 @@ public class Measurement
 {
 	private Integer time;
 	private static double temperature;
+	private ArrayList<Measurement> Measurement = new ArrayList<Measurement>();
 	
 	public Integer getTime() 
 	{
@@ -32,13 +33,13 @@ public class Measurement
 		this.temperature = temperature;
 	}
 	
-	static Double averageTemperature(ListMeasurement bl, Integer y) 
+	static Double averageTemperature(ArrayList<Measurement> bl,double startTime,double endTime) 
 	{ 
 		
 		Stream<Measurement> MeasurementStream = bl.stream();  
 		
 		Stream<Measurement> filteredMeasurementsStream = MeasurementStream
-				.filter((Measurement book) -> startTime >= Measurement.temperature && Measurement.temperature <= endTime); 
+				.filter((Measurement measurement) -> startTime >= measurement.temperature && measurement.temperature <= endTime); 
 		
 		DoubleStream MeasurementtemperatureStream = filteredMeasurementsStream.mapToDouble(Measurement -> Measurement.temperature); 
 				
